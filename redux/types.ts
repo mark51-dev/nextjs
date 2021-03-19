cbff32337586e2835636bcf5a68b16cd0ae8ddc0
mark51-dev <54667602+mark51-dev@users.noreply.github.com>
2021-03-19 15:15:33 +0200

    Update types.ts

--- redux/types.ts
+++ redux/types.ts
@@ -13,15 +13,5 @@ interface GetAllPosts {
     payload: Array<IPost>
 }
 
-interface AddPostAction {
-    type: PostActionTypes.ADD_POST,
-    payload: Array<IPost>
-}
 
-interface RemovePostAction {
-    type: PostActionTypes.REMOVE_POST,
-    payload: Array<IPost>
-}
-
-
-export type PostAction = AddPostAction | RemovePostAction | GetAllPosts
+export type PostAction =  GetAllPosts
